63b56ae304405d809badfbe756611884a1e8bc16
shivanjali kaname <shivanjali.kaname@atmecs.com>
2017-07-05 03:00:09 -0700

    second try

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,29 +1,19 @@
-#
-# Cookbook:: jenkins
-# Recipe:: default
-#
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-# yum_repository "jenkins" do
-#    description "repository for jenkins"
-#    baseurl "http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo"
-#    gpgkey "http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key"
-#    action :add
-# end
+execute 'jenkins.war' do
+	command 'wget https://updates.jenkins-ci.org/download/war/2.68/jenkins.war'
+#	not_if {File.exists?('/')}
+end
 
-  execute 'jenkins-repo' do
-   command 'rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key'
-  end
+execute 'jenkins-war' do
+	command 'java -Djenkins.install.runSetupWizard=false -jar jenkins.war'
+end
 
-  execute 'jenkins-repo' do
-   command 'wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo'
-  end 
+execute 'jenkinc-cli' do
+	command 'http://localhost:8080/jnlpJars/jenkins-cli.jar'
+	not_if {File.exists?('/root')}
+end
 
- yum_package "jenkins" do
-   action :install
- end
-
- service 'jenkins' do
-    action [:start, :enable]
- end
-
+execute 'jenkins-plugins' do
+	command 'java -jar jenkins-cli.jar -s http://localhost:8080 -auth shivanjali:fe1e7afe393ca299baf1446467fe7898 install-plugin Claim CppCheck Git Findbugs HGCA HTMLPublisher'
+end	
